54df52ddad22a4adc541f35afa42463e99983b8f
Daniele Tricoli <eriol@mornie.org>
2011-01-07 04:42:31 +0100

    Added salinity function

---
+++ src/oceanography.c
@@ -0,0 +1,30 @@
+#include <math.h>
+
+#include "oceanography.h"
+
+#define A(xt) (-3.107E-3 * (xt) + 0.4215)
+#define B(xt) ((4.464e-4 * (xt) + 3.426e-2) * (xt) + 1.0)
+#define C(xp) (((3.989e-15 * (xp) - 6.370e-10) * (xp) + 2.070e-5) * (xp))
+#define RT35(xt) ((((1.0031e-9 * (xt) - 6.9698e-7) * (xt) + 1.104259e-4) \
+                  * (xt) + 2.00564e-2) * (xt) + 0.6766097)
+
+static double _sal(double xr, double xt)
+{
+    return ((((2.7081 * xr - 7.0261) * xr + 14.0941) * xr + 25.3851)
+            * xr - 0.1692) * xr + 0.0080 + (xt / (1.0 + 0.0162 * xt)) *
+           (((((-0.0144 * xr + 0.0636) * xr - 0.0375) * xr - 0.0066)
+            * xr -0.0056) * xr + 0.0005);
+}
+
+double salinity(double conductivity, double temperature, double pressure)
+{
+    double corrected_temperature, rt;
+    corrected_temperature = temperature - 15.0;
+
+    rt = conductivity / (RT35(temperature) * (1.0 + C(pressure) /
+                                              (B(temperature) + A(temperature)
+                                              * conductivity)));
+    rt = sqrt(fabs(rt));
+
+    return _sal(rt, corrected_temperature);
+}
---
+++ src/oceanography.h
@@ -0,0 +1,6 @@
+#ifndef OCEANOGRAPHY_H
+#define OCEANOGRAPHY_H
+
+double salinity(double conductivity, double temperature, double pressure);
+
+#endif
