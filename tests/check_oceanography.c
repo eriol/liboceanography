f2a60c84f0d0c6ef5d2f67ed43e313bff067b401
Daniele Tricoli <eriol@mornie.org>
2011-01-11 00:37:03 +0100

    Added some tests for salinity function

---
+++ tests/check_oceanography.c
@@ -0,0 +1,45 @@
+#include <math.h>
+#include <stdlib.h>
+
+#include <check.h>
+
+#include "../src/oceanography.h"
+
+#define EPSILON 0.00001
+
+static int cmp_double(double x, double y)
+{
+    return fabs(x - y) < EPSILON;
+}
+
+
+START_TEST(test_salinity)
+{
+  ck_assert(cmp_double(salinity(1, 15, 0),  35.0));
+  ck_assert(cmp_double(salinity(1.888091, 40, 10000),  40.0));
+  ck_assert(cmp_double(salinity(1.2, 20, 2000),  37.245628));
+
+  ck_assert(cmp_double(salinity(5e-5, 15, 0),  0.0));
+}
+END_TEST
+
+Suite *oceanography_suite(void)
+{
+    Suite *s = suite_create("Oceanography");
+    TCase *tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_salinity);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+int main(void)
+{
+  int number_failed;
+  Suite *s = oceanography_suite();
+  SRunner *sr = srunner_create(s);
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
