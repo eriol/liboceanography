69b4706be6779c9181060824a778f407673fca45
Daniele Tricoli <eriol@mornie.org>
2011-01-15 07:04:34 +0100

    Small change in header license comment

--- src/oceanography.h
+++ src/oceanography.h
@@ -1,5 +1,4 @@
-/* oceanography.h -- liboceanography header.
- *
+/*
  * Copyright 2011 Daniele Tricoli <eriol@mornie.org>
  *
  * This file is part of liboceanography.
@@ -16,6 +15,9 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library. If not, see http://www.gnu.org/licenses/.
+ *
+ *
+ * oceanography.h -- liboceanography header.
  */
 
 #ifndef OCEANOGRAPHY_H
--- tests/test_oceanography.c
+++ tests/test_oceanography.c
@@ -1,5 +1,4 @@
-/* test_oceanography.c -- Unit tests for liboceanography.
- *
+/*
  * Copyright 2011 Daniele Tricoli <eriol@mornie.org>
  *
  * This file is part of liboceanography.
@@ -16,6 +15,9 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library. If not, see http://www.gnu.org/licenses/.
+ *
+ *
+ * test_oceanography.c -- Unit tests for liboceanography.
  */
 
 #include <math.h>
